7bddc4d8033912b582412b506384a3f9169da29e
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-03-26 16:10:34 -0300

    feat: Implement LogIn activity with Firebase authentication

--- app/src/main/java/com/example/barbeariaapp/View/LogInActivity.kt
+++ app/src/main/java/com/example/barbeariaapp/View/LogInActivity.kt
@@ -1,21 +1,51 @@
 package com.example.barbeariaapp.View
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.barbeariaapp.R
+import com.example.barbeariaapp.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_log_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        binding.btnEntrar.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){ task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }else{
+                        Toast.makeText(this, "Falha no login: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        binding.tvPossuiConta.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        binding.icVoltar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
